bf4c94d1f36f30697dc9954ad20a41082b1e2f83
Duck Deux <sikerbela@riseup.net>
2016-11-21 11:19:06 +0000

    draw on pixmap
    still dysfunctional

--- config.h
+++ config.h
@@ -1,4 +1,5 @@
 /* I read this much from stdin. */
 #define MAX 1024
 
-static char font[] = "helvetica:size=11";
+static char font[] = "helvetica:size=13";
+static XRenderColor color = {0xFFFF, 0x0000, 0x0000, 0xFFFF};
--- drwr.c
+++ drwr.c
@@ -2,41 +2,46 @@
 #include <unistd.h>
 
 #include <X11/Xlib.h>
+#include <X11/Xatom.h>
 #include <X11/Xft/Xft.h>
 
 #include "config.h"
 
 int main()
 {
-	char buf[MAX+1];
-	int x, s;
+	char buf[MAX+1] = "Lorem Ipsum";
+	int x, s, w, h;
 	Display * d;
 	Window r;
 	XftFont * f;
 	XftDraw * drw;
-	XRenderColor xrc;
 	XftColor xftc;
-	
-	x = read(0, buf, MAX);
-	buf[x] = '\0';
-	printf("drwr: read %s\n", buf);
+	Pixmap pix;
 
 	if (!(d = XOpenDisplay(NULL))) fprintf(stderr, "drwr: cannot open display.\n");
 	s = DefaultScreen(d);
 	r = RootWindow(d, s);
 	if (!(f = XftFontOpenName(d, s, font))) fprintf(stderr, "drwr: could not open font.\n");
-	
-	drw = XftDrawCreate(d, r, DefaultVisual(d, s), DefaultColormap(d, s));
-	xrc.red = 0xFFFF;
-	xrc.green = 0;
-	xrc.blue = 0;
-	xrc.alpha = 0xFFFF;
-	XftColorAllocValue(d, DefaultVisual(d, s), DefaultColormap(d, s), &xrc, &xftc);
-	XftDrawRect(drw, &xftc, 200, 200, 50, 50);
-	XftDrawStringUtf8(drw, &xftc, f, 50, 50, (XftChar8 *)buf, x);
 
+	/*
+	x = read(0, buf, MAX);
+	buf[x] = '\0';
+	printf("drwr: read %s\n", buf);
+	*/
+	x = 12;
+
+	w = 1600;
+	h = 900;
+	if(!(pix = XCreatePixmap(d, r, w, h, DefaultDepth(d, s)))) fprintf(stderr, "drwr: could not create pixmap.\n");
+
+	drw = XftDrawCreateAlpha(d, pix, DefaultDepth(d, s));
+	XftColorAllocValue(d, DefaultVisual(d, s), DefaultColormap(d, s), &color, &xftc);
+	XftDrawStringUtf8(drw, &xftc, f, 100, 100, (XftChar8 *)buf, x);
+	XftDrawRect(drw, &xftc, 10, 10, 50, 50);
+
+	XSetWindowBackgroundPixmap(d, r, pix);
 	XFlush(d);
-	XSetCloseDownMode(d, RetainPermanent);
+
 	XCloseDisplay(d);
 
 	return 0;
